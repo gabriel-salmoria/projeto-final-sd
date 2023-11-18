ac874236ecbb5acbd36927ecdcb10318be3aff6c
Prog-in <joaogabrielferes@gmail.com>
2023-11-18 18:04:39 -0300

    format adder and testbench files

--- somador.vhd
+++ somador.vhd
@@ -3,25 +3,26 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
 ENTITY somador IS
-GENERIC (
-N : INTEGER);
-PORT (
-	A : IN std_logic_vector(N - 1 DOWNTO 0);
-	B: IN std_logic_vector(N - 1 DOWNTO 0);
-	sum : OUT std_logic_vector(N - 1 DOWNTO 0));
+	GENERIC (
+		N : INTEGER := 64
+	);
+	PORT (
+		A   : IN std_logic_vector(N - 1 DOWNTO 0);
+		B   : IN std_logic_vector(N - 1 DOWNTO 0);
+		sum : OUT std_logic_vector(N - 1 DOWNTO 0)
+	);
 END somador;
 
 ARCHITECTURE rtl OF somador IS
-signal add1, add2: unsigned(N - 1 downto 0);
-signal resultado: unsigned(N - 1 downto 0);
-BEGIN
-process(A, B)
-begin
-add1 <= unsigned(A);
-add2 <= unsigned(B);
-end process;
-resultado <= add1 + add2;
-
-
-sum <= std_logic_vector(resultado);
+	SIGNAL add1, add2 : unsigned(N - 1 DOWNTO 0);
+	SIGNAL resultado  : unsigned(N - 1 DOWNTO 0);
+	BEGIN
+		PROCESS(A, B)
+		BEGIN
+			add1 <= unsigned(A);
+			add2 <= unsigned(B);
+		END PROCESS;
+		
+		resultado <= add1 + add2;
+		sum <= std_logic_vector(resultado);
 END rtl;
--- testbench_add.vhd
+++ testbench_add.vhd
@@ -1,11 +1,11 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
-use ieee.numeric_std.all;
-use IEEE.std_logic_textio.all;
-use std.textio.all;
+USE ieee.numeric_std.ALL;
+USE IEEE.std_logic_textio.ALL;
+USE std.textio.ALL;
 
 ENTITY testbench_add IS
-end testbench_add;
+END testbench_add;
 
 architecture circuito of testbench_add is
 	component somador IS
@@ -19,58 +19,53 @@ architecture circuito of testbench_add is
 		);
 	END component;
 	
-	signal A   : std_logic_vector(63 DOWNTO 0);
-	signal B   : std_logic_vector(63 DOWNTO 0);
-	signal sum : std_logic_vector(63 DOWNTO 0);
+	SIGNAL A   : std_logic_vector(63 DOWNTO 0);
+	SIGNAL B   : std_logic_vector(63 DOWNTO 0);
+	SIGNAL sum : std_logic_vector(63 DOWNTO 0);
 		
 	CONSTANT wait_time : TIME := 10 ns;
-	begin
+	BEGIN
 		DUT: somador 
-		GENERIC MAP (
-			N => 64
-		)
-		PORT MAP (
-			A   => A,
-			B   => B,
-			sum => sum
-		);
+			GENERIC MAP (
+				N => 64
+			)
+			PORT MAP (
+				A   => A,
+				B   => B,
+				sum => sum
+			);
 		
 		stim: PROCESS IS
-			file goldenmodel   : text open read_mode is "/home/joao/projeto-final-sd/estimulos-add.dat";
+			file goldenmodel   : text OPEN read_mode IS "/home/joao/projeto-final-sd/estimulos-add.dat";
 			variable curr_line : line;
 			variable space     : character;
-			variable A_value   : bit_vector(63 downto 0);
-			variable B_value   : bit_vector(63 downto 0);
-			variable result    : bit_vector(63 downto 0);
+			variable A_value   : bit_vector(63 DOWNTO 0);
+			variable B_value   : bit_vector(63 DOWNTO 0);
+			variable result    : bit_vector(63 DOWNTO 0);
 
 			BEGIN			
-				while not endfile(goldenmodel) loop
+				WHILE not endfile(goldenmodel) LOOP
+					-- first line: A & " " & B
 					readline(goldenmodel, curr_line);
 					read(curr_line, A_value);
 					read(curr_line, space);
 					read(curr_line, B_value);
+					-- second line: A + B (64 bits)
+					readline(goldenmodel, curr_line);
+					read(curr_line, result);
 					
 					A <= to_stdlogicvector(A_value);
 					B <= to_stdlogicvector(B_value);
-
-					readline(goldenmodel, curr_line);
-					read(curr_line, result);
 					
 					wait for wait_time;
 					
 					ASSERT (to_stdlogicvector(result) = sum)
-					REPORT 
-						"Soma incorreta: " 
-						& integer'image(to_integer(unsigned(sum)))
-						& " != "
-						& integer'image(to_integer(unsigned(to_stdlogicvector(result))))
-					SEVERITY error;
-					
+					REPORT "Soma incorreta." SEVERITY error;
 				END LOOP;
 				
-				ASSERT false REPORT "Test done." SEVERITY note;
+				ASSERT false REPORT "Teste encerrado." SEVERITY note;
 			
 				WAIT;
 			
 	END PROCESS;
-end circuito;
+END circuito;
